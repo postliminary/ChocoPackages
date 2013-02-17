d95d2c2c90520a21e75d5418aca40547c794d4ae
Jean-Philippe Steinmetz <caskater47@gmail.com>
2013-02-17 14:31:31 -0800

    Adding AnyDVD to chocolatey packages

---
+++ anydvd/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'anydvd'
+$installerType = 'EXE'
+$url = 'http://static.slysoft.com/SetupAnyDVD7150.exe'
+$url64 = $url
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
