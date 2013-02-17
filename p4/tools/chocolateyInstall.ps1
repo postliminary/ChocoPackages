ce85dd2e4a2b5fc13580019747974eb4e1db572e
Jean-Philippe Steinmetz <caskater47@gmail.com>
2013-02-17 14:11:52 -0800

    Adding chocolatey packages for P4 and P4SCC

---
+++ p4/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'p4'
+$installerType = 'EXE'
+$url = 'http://www.perforce.com/downloads/perforce/r12.2/bin.ntx86/perforce.exe'
+$url64 = 'http://www.perforce.com/downloads/perforce/r12.2/bin.ntx64/perforce64.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
