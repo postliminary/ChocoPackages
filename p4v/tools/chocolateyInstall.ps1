c403eed6bc45e0f77593e5aa6a888c77d47fd1a8
Jean-Philippe Steinmetz <caskater47@gmail.com>
2013-02-17 13:34:19 -0800

    Adding initial chocolatey package for p4v 2012.3

---
+++ p4v/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'p4v'
+$installerType = 'EXE'
+$url = 'http://www.perforce.com/downloads/perforce/r12.3/bin.ntx86/p4vinst.exe'
+$url64 = 'http://www.perforce.com/downloads/perforce/r12.3/bin.ntx64/p4vinst64.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
